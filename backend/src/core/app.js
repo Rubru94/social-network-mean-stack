facc5e52ebe2fb7d09d8b2432210e2359831365d
Ruben Rubio <rubenru94@hotmail.com>
2022-11-23 17:10:52 +0100

    feat: Create web server using express

---
+++ backend/src/core/app.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const { Aggregate } = require('mongoose');
+
+const app = express();
+config();
+
+function config() {
+    app.use(bodyParser.json({ limit: '1mb' }));
+    app.use(bodyParser.text({ type: 'text/html' }));
+    app.use(cors());
+    app.use(bodyParser.urlencoded({ extended: false }));
+}
+
+app.get('/', (req, res) => {
+    res.status(200).send({ msg: 'hello world !' });
+});
+
+module.exports = app;
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,29 @@
 'use strict';
 
+const dotenvFlow = require('dotenv-flow');
+const http = require('http');
 const mongoose = require('mongoose');
+const moduleAlias = require('module-alias');
+
+dotenvFlow.config({ path: 'environments' });
+moduleAlias.addAliases({
+    '@core': `${__dirname}/core`,
+    '@utils': `${__dirname}/utils`,
+    '@root': __dirname
+});
+
+const app = require('@core/app.js');
+const host = process.env.APP_HOST;
+const port = process.env.APP_PORT;
 
 mongoose.Promise = global.Promise;
 mongoose
     .connect('mongodb://localhost:27017/social_network_mean', { useNewUrlParser: true, useUnifiedTopology: true })
-    .then((db) => console.log('Db is connected'))
+    .then(async (db) => {
+        const server = http.createServer(app);
+        server.listen(port, +host, () => {
+            console.info(`Server listening on host: ${host} and port ${port}`);
+        });
+        console.log('Db is connected');
+    })
     .catch((err) => console.error(err));

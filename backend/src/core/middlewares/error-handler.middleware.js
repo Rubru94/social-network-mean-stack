942d9527cf2cfa72bbee946acc105c4776f44e16
Ruben Rubio <rubenru94@hotmail.com>
2022-11-29 17:06:29 +0100

    fix: Correct error handler middleware

--- backend/src/core/middlewares/error-handler.middleware.js
+++ backend/src/core/middlewares/error-handler.middleware.js
@@ -14,9 +14,7 @@ const error = require('@core/models/error.model');
 const handleError = (err, req, res, next) => {
     if (res.headersSent) return;
     if (!(err instanceof error.CustomError)) err = new error.CustomError(err);
-    let status;
-    if (err.statusCode) status = err.statusCode;
-    res.status(status ?? http2.constants.HTTP_STATUS_INTERNAL_SERVER_ERROR).json(err);
+    res.status(err.statusCode ?? http2.constants.HTTP_STATUS_INTERNAL_SERVER_ERROR).json(err);
 };
 
 module.exports = handleError;

347e47bbac74214db9bd59f8144d03ed6772bacd
Ruben Rubio <rubenru94@hotmail.com>
2022-12-01 16:57:50 +0100

    feat: Add home template

--- frontend/src/app/public/components/home/home.component.ts
+++ frontend/src/app/public/components/home/home.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+    selector: 'app-home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+    title: string;
 
+    constructor() {
+        this.title = 'Welcome to Social Network!!';
+    }
 }

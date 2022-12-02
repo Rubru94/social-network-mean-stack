ffd40d10ba6a4be80e7ecd97fc2e60ca73bf7ce5
Ruben Rubio <rubenru94@hotmail.com>
2022-12-02 11:03:05 +0100

    feat: Define component user-settings

--- frontend/src/app/private/components/user-settings/user-settings.component.spec.ts
+++ frontend/src/app/private/components/user-settings/user-settings.component.spec.ts
@@ -3,21 +3,20 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { UserSettingsComponent } from './user-settings.component';
 
 describe('UserSettingsComponent', () => {
-  let component: UserSettingsComponent;
-  let fixture: ComponentFixture<UserSettingsComponent>;
+    let component: UserSettingsComponent;
+    let fixture: ComponentFixture<UserSettingsComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ UserSettingsComponent ]
-    })
-    .compileComponents();
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [UserSettingsComponent]
+        }).compileComponents();
 
-    fixture = TestBed.createComponent(UserSettingsComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+        fixture = TestBed.createComponent(UserSettingsComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
--- frontend/src/app/private/components/user-settings/user-settings.component.ts
+++ frontend/src/app/private/components/user-settings/user-settings.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { User } from 'src/app/public/models/user.model';
+import { UserService } from 'src/app/public/services/user.service';
 
 @Component({
-  selector: 'app-user-settings',
-  templateUrl: './user-settings.component.html',
-  styleUrls: ['./user-settings.component.scss']
+    selector: 'app-user-settings',
+    templateUrl: './user-settings.component.html',
+    styleUrls: ['./user-settings.component.scss']
 })
 export class UserSettingsComponent {
+    title: string;
+    token: string;
+    user: User;
 
+    constructor(private router: Router, private userService: UserService) {
+        this.title = 'User settings';
+        this.token = this.userService.token;
+        this.user = this.userService.identity;
+    }
+
+    ngOnInit() {}
 }

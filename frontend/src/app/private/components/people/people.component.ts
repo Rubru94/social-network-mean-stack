dafb04674165ae73207cabbb6acb765fa3d3d9b9
Ruben Rubio <rubenru94@hotmail.com>
2022-12-05 14:12:51 +0100

    feat: User list with pagination

--- frontend/src/app/private/components/people/people.component.ts
+++ frontend/src/app/private/components/people/people.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { UserHttpService } from 'src/app/public/http/user.http.service';
 import { FormStatus } from 'src/app/public/models/form-status.model';
 import { User } from 'src/app/public/models/user.model';
+import { environment } from '../../../../../environments/env';
 
 @Component({
     selector: 'app-people',
@@ -12,20 +13,28 @@ import { User } from 'src/app/public/models/user.model';
 export class PeopleComponent implements OnInit {
     title: string;
     currentPage: number;
+    previousPage: number;
+    nextPage: number;
+    totalPages: number;
     users: User[];
     status: FormStatus;
     errMsg?: string;
 
+    api: string;
+
     constructor(private activatedRoute: ActivatedRoute, private router: Router, private userHttpService: UserHttpService) {
         this.title = 'People';
         this.currentPage = 1;
+        this.previousPage = 0;
+        this.nextPage = 0;
+        this.totalPages = 1;
         this.users = [];
         this.status = FormStatus.None;
+        this.api = `${environment.apiURL}/api`;
     }
 
     async ngOnInit(): Promise<void> {
-        this.currentPage = await this.actualPage();
-        this.getUsers(this.currentPage);
+        this.actualPage();
         console.log(this.currentPage);
     }
 
@@ -33,15 +42,19 @@ export class PeopleComponent implements OnInit {
         return FormStatus;
     }
 
-    async actualPage(): Promise<number> {
-        return new Promise((resolve, reject) => {
-            this.activatedRoute.params.subscribe({
-                next: (params) => resolve(+params['page']),
-                error: (err: Error) => {
-                    this.status = FormStatus.Invalid;
-                    this.errMsg = err.message;
-                }
-            });
+    actualPage(): void {
+        this.activatedRoute.params.subscribe({
+            next: (params) => {
+                const page = +params['page'];
+                this.currentPage = page;
+                this.nextPage = page + 1;
+                this.previousPage = page - 1 > 0 ? page - 1 : 1;
+                this.getUsers(this.currentPage);
+            },
+            error: (err: Error) => {
+                this.status = FormStatus.Invalid;
+                this.errMsg = err.message;
+            }
         });
     }
 
@@ -50,6 +63,7 @@ export class PeopleComponent implements OnInit {
             next: (res: { users: User[]; total: number; pages: number }) => {
                 console.log(res);
                 this.users = res.users;
+                this.totalPages = res.pages;
                 if (res.pages && page > res.pages) {
                     this.router.navigateByUrl('/private/people');
                     this.ngOnInit();

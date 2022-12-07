3e5a1bbfdddab0930821ce10d6d2c9e8811f3b8c
Ruben Rubio <rubenru94@hotmail.com>
2022-12-07 12:02:07 +0100

    feat: Add publication form to user-sidebar component

--- frontend/src/app/private/components/user-sidebar/user-sidebar.component.ts
+++ frontend/src/app/private/components/user-sidebar/user-sidebar.component.ts
@@ -1,7 +1,9 @@
 import { Component, DoCheck, OnInit } from '@angular/core';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { CounterSet } from 'src/app/public/models/counter-set.model';
 import { FormStatus } from 'src/app/public/models/form-status.model';
+import { Publication } from 'src/app/public/models/publication.model';
 import { User } from 'src/app/public/models/user.model';
 import { UserService } from 'src/app/public/services/user.service';
 import { environment } from '../../../../../environments/env';
@@ -17,13 +19,30 @@ export class UserSidebarComponent implements OnInit, DoCheck {
     token?: string;
     identity?: User;
     api: string;
+    publication: Publication;
     status: FormStatus;
     errMsg?: string;
 
-    constructor(private router: Router, private userService: UserService) {
+    publicationForm: FormGroup;
+    fileInput?: File | null;
+
+    constructor(private fb: FormBuilder, private router: Router, private userService: UserService) {
         this.title = 'User data';
+        this.publication = new Publication();
         this.api = `${environment.apiURL}/api`;
         this.status = FormStatus.None;
+        this.publicationForm = this.fb.group({});
+    }
+
+    get publicationFormControl(): { [key: string]: AbstractControl } {
+        return this.publicationForm.controls;
+    }
+
+    isInvalidControl(field: string): boolean {
+        return (
+            this.publicationFormControl[field].invalid &&
+            (this.publicationFormControl[field].dirty || this.publicationFormControl[field].touched)
+        );
     }
 
     get FormStatus(): typeof FormStatus {
@@ -38,11 +57,39 @@ export class UserSidebarComponent implements OnInit, DoCheck {
         this.token = this.userService.token;
         this.identity = this.userService.identity;
         this.counterSet = this.userService.counterSet;
+
+        this.publicationForm = this.fb.group({
+            text: new FormControl('', [Validators.required]),
+            file: new FormControl()
+        });
     }
 
     ngDoCheck(): void {
         this.token = this.userService.token;
         this.identity = this.userService.identity;
         this.counterSet = this.userService.counterSet;
+    }
+
+    onSubmit(form: FormGroup): void {
+        this.publication = new Publication({ ...form.value, ...{ user: this.identity?._id } });
+        console.log(this.publication);
+        /*
+      this.userHttpService.login(this.user).subscribe({
+          next: (res: User | { token: string }) => {
+              localStorage.setItem('identity', JSON.stringify(res));
+              this.requestToken();
+              this.status = FormStatus.Valid;
+              form.reset();
+          },
+          error: (err: Error) => {
+              this.status = FormStatus.Invalid;
+              this.errMsg = err.message;
+          }
+      }); */
+    }
+
+    onFileSelected(event: Event) {
+        const element = event.currentTarget as HTMLInputElement;
+        this.fileInput = element.files?.[0];
     }
 }
--- frontend/src/app/public/models/publication.model.ts
+++ frontend/src/app/public/models/publication.model.ts
@@ -1,17 +1,17 @@
 import { User } from './user.model';
 
 export class Publication {
-    id: string;
+    _id: string;
     user: string | User;
     text: string;
     file: string;
     createdAt: Date;
 
-    constructor(publication: { _id: string; user: string | User; text: string; file: string; createdAt: Date }) {
-        this.id = publication._id;
-        this.user = publication.user;
-        this.text = publication.text;
-        this.file = publication.file;
-        this.createdAt = publication.createdAt;
+    constructor(publication?: Partial<Publication>) {
+        this._id = publication?._id ?? '';
+        this.user = publication?.user ?? '';
+        this.text = publication?.text ?? '';
+        this.file = publication?.file ?? '';
+        this.createdAt = publication?.createdAt ?? new Date();
     }
 }

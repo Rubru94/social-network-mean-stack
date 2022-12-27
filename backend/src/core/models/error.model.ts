59384c6a1eefede4698f976bbb41a07e5e73d283
Ruben Rubio <rubenru94@hotmail.com>
2022-12-27 11:44:55 +0100

    fix: Show error toString if it exists & has no message

--- backend/src/core/models/error.model.ts
+++ backend/src/core/models/error.model.ts
@@ -8,7 +8,7 @@ export class CustomError extends Error {
     constructor(error?: Partial<CustomError>) {
         super();
         this.statusCode = error?.statusCode ?? constants.HTTP_STATUS_INTERNAL_SERVER_ERROR;
-        this.error = error?.error ?? error?.message ?? 'Unhandled error';
+        this.error = error?.error ?? error?.message ?? error?.toString() ?? 'Unhandled error';
     }
 }
 
